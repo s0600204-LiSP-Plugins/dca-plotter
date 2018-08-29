fc926a031027cda779138a9978c4c25e9abf3b51
s0600204 <dev@s0600204>
2018-08-29 18:04:51 +0100

    Add function `get_mic_assign_name`

--- utilities.py
+++ utilities.py
@@ -1,4 +1,15 @@
 
+# get_mic_assign_name
+from lisp.plugins import get_plugin
+
 def build_default_mic_name(num):
     # TODO: translate string
     return "Microphone {0}".format(num)
+
+def get_mic_assign_name(numid):
+    inputs = get_plugin('DcaPlotter').SessionConfig['inputs']
+
+    return '{id} : {name}'.format_map({
+        'id': numid,
+        'name': inputs[numid - 1]['name'] if inputs else build_default_mic_name(numid)
+    })
