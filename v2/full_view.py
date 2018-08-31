ac2479d3ebc8f2c986a5e786e9baddd9a8583423
s0600204 <dev@s0600204>
2018-08-31 19:39:16 +0100

    Continue custom model view

--- v2/full_view.py
+++ v2/full_view.py
@@ -1,12 +1,20 @@
 
-from PyQt5.QtCore import QModelIndex # Qt
-#from PyQt5.QtGui import QMouseEvent
-from PyQt5.QtWidgets import QAbstractItemView
+from PyQt5.QtCore import Qt, QModelIndex, QRect
+from PyQt5.QtGui import QFontMetrics, QPainter, QPen # QMouseEvent
+from PyQt5.QtWidgets import QAbstractItemView, QApplication
 
 class PlotterView(QAbstractItemView):
 
+    CUEROW_MARGIN = 8
+
+    _cueid_width = 0
+    _cuerow_height = []
+    _cuerow_height_dirty = []
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.setFont(QApplication.font("QTreeView"));
+        self._fontmetrics = QFontMetrics(self.font())
 
     #def dataChanged(topLeft, bottomRight):
     #    '''This slot is called when the items with model indexes in the rectangle from topLeft to bottomRight change
@@ -19,7 +27,8 @@ class PlotterView(QAbstractItemView):
         '''Returns the view's horizontal offset.
         @return int
         '''
-        return 0
+        #return self.horizontalScrollBar().value()
+        return 0 # Intention is to scale dca blocks horizontally so we'll never need to scroll horizontally
 
     def indexAt(self, point): # REQUIRED REQUESTED
         '''Returns the model index of the item at position point in the view's viewport
@@ -33,6 +42,7 @@ class PlotterView(QAbstractItemView):
     #    @arg index QModelIndex
     #    @return bool
     #    '''
+    #    return False
 
     #def mousePressEvent(self, event):
     #    '''Typically used to set the current model index to the index of the clicked item
@@ -47,10 +57,60 @@ class PlotterView(QAbstractItemView):
         '''
         return QModelIndex()
 
-    #def paintEvent(self, event):
-    #    '''Paints the view's contents on the viewport
-    #    @arg event QPaintEvent
-    #    '''
+    def paintEvent(self, event):
+        '''Paints the view's contents on the viewport
+        @arg event QPaintEvent
+        '''
+        painter = QPainter(self.viewport())
+
+        # @todo: calculate (and set) this elsewhere
+        self._cueid_width = self._fontmetrics.height() * 3
+
+        # Update/Recalculate dimensions
+        self._recalculate_cuerow_heights()
+
+        viewport_y = 0
+
+        for row_num in range(self.model().childCount(self.rootIndex())):
+            row_x = 0
+            row_y = viewport_y
+            row_index = self.model().index(row_num, 0, self.rootIndex())
+
+            # Draw the Cue Number & Name (Haven't decided what I actually want here yet)
+            row_viewoptions = self.viewOptions()
+            row_viewoptions.rect = QRect(0, row_y, self.viewport().width(), self._fontmetrics.height())
+            self.itemDelegate().paint(painter, row_viewoptions, row_index)
+            viewport_y += self._fontmetrics.height()
+
+            # Draw the DCA blocks
+            block_x = self._cueid_width
+            block_width = (self.viewport().width() - self._cueid_width) / self.model().childCount(row_index)
+            for block_num in range(self.model().childCount(row_index)):
+                block_y = viewport_y
+                block_index = self.model().index(block_num, 0, row_index)
+
+                for assign_num in range(self.model().childCount(block_index)):
+                    assign_index = self.model().index(assign_num, 0, block_index)
+                    assign_viewoptions = self.viewOptions()
+                    assign_viewoptions.rect = QRect(block_x, block_y, block_width, self._fontmetrics.height())
+                    self.itemDelegate().paint(painter, assign_viewoptions, assign_index)
+                    block_y += self._fontmetrics.height()
+
+                # Draw a rectangle round the block
+                block_outline_rect = QRect(block_x, viewport_y, block_width, self._cuerow_height[row_num])
+                self._paint_outline(painter, block_outline_rect)
+                block_x += block_width
+
+            viewport_y += self._cuerow_height[row_num]
+
+            # Draw a rectangle around the entire cue
+            # Temporary - probably will remove or replace this
+            # However, for debugging, so we have a visual guide of where things are being drawn
+            outline_rect = QRect(row_x, row_y, self.viewport().width(), viewport_y - row_y)
+            self._paint_outline(painter, outline_rect)
+
+            viewport_y += self.CUEROW_MARGIN
+
 
     #def resizeEvent(self, event):
     #    '''Typically used to update the scrollbars
@@ -83,10 +143,13 @@ class PlotterView(QAbstractItemView):
     #    @arg hint QAbstractItemView::ScrollHint
     #    '''
 
-    #def setModel(self, model):
-    #    '''Makes the view use the given model
-    #    @arg model QAbstractItemModel
-    #    '''
+    def setModel(self, model):
+        '''Makes the view use the given model
+        @arg model QAbstractItemModel
+        '''
+        super().setModel(model)
+        self._cuerow_height_dirty = [rownum for rownum in range(self.model().childCount(self.rootIndex()))]
+        self._cuerow_height = [0 for rownum in range(self.model().childCount(self.rootIndex()))]
 
     #def setSelection(self, rect, flags): # REQUIRED
     #    '''Applies the selection flags to all of the items in or touching the rectangle rect
@@ -103,9 +166,9 @@ class PlotterView(QAbstractItemView):
         '''Returns the view's vertical offset
         @return int
         '''
-        return 0
+        return self.verticalScrollBar().value()
 
-    #def visualRect(self, index): # REQUIRED
+    #def visualRect(self, index): # REQUIRED REQUESTED
     #    '''Returns the rectangle occupied by the item at the given model index
     #    @arg index QModelIndex
     #    @return QRect
@@ -117,3 +180,33 @@ class PlotterView(QAbstractItemView):
     #    @return QRegion
     #    '''
 
+    def _mark_rowheight_dirty(self, rownum):
+        self._cuerow_height_dirty.append(rownum)
+
+    def _recalculate_cuerow_heights(self):
+        while len(self._cuerow_height_dirty):
+            entry_count = 0
+            row_num = self._cuerow_height_dirty[0]
+            row_index = self.model().index(row_num, 0, self.rootIndex())
+
+            for block_num in range(self.model().childCount(row_index)):
+                block_index = self.model().index(0, block_num, row_index)
+                entry_count = max(entry_count, self.model().childCount(block_index))
+
+            self._cuerow_height[row_num] = max(1, entry_count) * self._fontmetrics.height()
+            self._cuerow_height_dirty.remove(row_num)
+
+
+    def _paint_outline(self, painter, rect):
+        rect = rect.adjusted(0, 0, -1, -1);
+        painter.save();
+        painter.setPen(QPen(self.palette().dark().color(), 0.5));
+        painter.drawRect(rect);
+        painter.restore();
+
+    def _paint_line(self, painter, rect):
+        rect = rect.adjusted(0, 0, -1, -1);
+        painter.save();
+        painter.setPen(QPen(self.palette().dark().color(), 0.5));
+        painter.drawLine(rect.topLeft(), rect.bottomRight());
+        painter.restore();
