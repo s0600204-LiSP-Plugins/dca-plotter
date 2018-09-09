2c4a82af8cfcd68b0d28be109ceff004845f265b
s0600204 <dev@s0600204>
2018-09-09 05:57:46 +0100

    Separate out reusable parts of the mapper model so they can be reused

--- mapper/model.py
+++ mapper/model.py
@@ -1,109 +1,17 @@
 
 import copy
-import enum
 
 # pylint: disable=no-name-in-module
-from PyQt5.QtCore import Qt, QAbstractItemModel, QModelIndex
-from PyQt5.QtGui import QBrush, QFont
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QModelIndex, Qt
 
-from lisp.plugins import get_plugin
-from lisp.plugins.dca_plotter.utilities import get_mic_assign_name
+from lisp.plugins.dca_plotter.model_primitives import AssignStateEnum, DcaModelTemplate, \
+    ModelsRow, ModelsEntry
 
-class AssignStateEnum(enum.Enum):
-    ASSIGN = enum.auto()
-    UNASSIGN = enum.auto()
-    NONE = enum.auto()
-
-### ABSTRACTS
-class DcaMapNode():
-    '''Abstract parent class'''
-    def __init__(self, parent=None):
-        self.parent = parent
-        self.flags = Qt.ItemFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
-
-    def data(self, role=Qt.DisplayRole):
-        # pylint: disable=no-self-use, unused-argument
-        return None
-
-    def index(self):
-        return self.model().createIndex(self.rownum(), 0, self)
-
-    def model(self):
-        return self.parent.model()
-
-    def next_sibling(self):
-        if self.rownum() < len(self.parent) - 1:
-            return self.parent.children[self.rownum() + 1]
-        return None
-
-    def prev_sibling(self):
-        if self.rownum():
-            return self.parent.children[self.rownum() - 1]
-        return None
-
-    def rownum(self):
-        return self.parent.children.index(self)
-
-class DcaMapBranchNode(DcaMapNode):
-    '''Branch parent class'''
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.children = []
-
-    def addChild(self, child):
-        self.children.append(child)
-
-    def child(self, child_num):
-        return self.children[child_num]
-
-    def childCount(self):
-        return len(self.children)
-
-    def getChildValues(self):
-        indexes = []
-        for child in self.children:
-            indexes.append(child.value())
-        return indexes
-
-    def getInsertPoint(self, new_value):
-        curr_values = self.getChildValues()
-        curr_values.append(new_value)
-        curr_values.sort()
-        return curr_values.index(new_value)
-
-    def removeChild(self, row):
-        return self.children.pop(row)
-
-class DcaMapLeafNode(DcaMapNode):
-    '''Leaf parent class'''
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.flags |= Qt.ItemNeverHasChildren
-
-    def childCount(self):
-        # pylint: disable=no-self-use
-        return 0
-
-### BRANCHES
-class DcaMapRootNode(DcaMapBranchNode):
-    '''Root class'''
-    def __init__(self, model, **kwargs):
-        super().__init__(**kwargs)
-        self._model = model
-
-    def model(self):
-        return self._model
-
-class DcaMapRow(DcaMapBranchNode):
-    '''Row class.'''
+class ModelsCueRow(ModelsRow):
+    '''Cue row class.'''
     def __init__(self, cue, **kwargs):
         super().__init__(**kwargs)
         self.cue = cue
-
-        # pylint: disable=unused-variable
-        for dca in range(get_plugin('DcaPlotter').SessionConfig['dca_count']):
-            self.addChild(DcaMapBlock(parent=self))
 
     def data(self, role=Qt.DisplayRole):
         if role == Qt.DisplayRole:
@@ -113,72 +21,7 @@ class DcaMapRow(DcaMapBranchNode):
     def value(self):
         return self.cue.index
 
-class DcaMapBlock(DcaMapBranchNode):
-    '''Block class'''
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.name = False
-
-    def addChild(self, child):
-        self.children.insert(self.getInsertPoint(child.value()), child)
-
-    def data(self, role=Qt.DisplayRole):
-        if role == Qt.DisplayRole:
-            return 'DCA {}'.format(self.parent.children.index(self) + 1) # @todo: Proper implementation of DCA names
-
-        return super().data(role)
-
-### LEAVES
-class DcaMapEntry(DcaMapLeafNode):
-    '''Entry class'''
-    def __init__(self, value, state=AssignStateEnum.NONE, **kwargs):
-        super().__init__(**kwargs)
-        self._value = value
-        self._is_inherited = state == AssignStateEnum.NONE
-        self._assign_state = state
-
-    def data(self, role=Qt.DisplayRole):
-        if role == Qt.DisplayRole:
-            name = get_mic_assign_name(self.value())
-            if self._assign_state == AssignStateEnum.NONE:
-                return "({})".format(name)
-            return name
-
-        if role == Qt.EditRole:
-            return self.value
-
-        if role == Qt.ForegroundRole: # Text colour:
-            if self._assign_state == AssignStateEnum.ASSIGN:
-                return QBrush(Qt.green)
-            if self._assign_state == AssignStateEnum.UNASSIGN:
-                return QBrush(Qt.red)
-            if self._assign_state == AssignStateEnum.NONE:
-                return QBrush(QApplication.palette().dark().color())
-
-        if role == Qt.FontRole and self._assign_state == AssignStateEnum.UNASSIGN:
-            font = QFont()
-            font.setStrikeOut(True)
-            return font
-
-        return super().data(role)
-
-    def assign_state(self):
-        return self._assign_state
-
-    def inherited(self):
-        return self._is_inherited
-
-    def setInherited(self, is_inherited):
-        self._is_inherited = is_inherited
-
-    def value(self):
-        return self._value
-
-### MODEL
-class DcaMappingModel(QAbstractItemModel):
-    def __init__(self):
-        super().__init__()
-        self.root = DcaMapRootNode(model=self)
+class DcaMappingModel(DcaModelTemplate):
 
     def amend_cuerow(self, cue, property_name, property_value):
         if property_name != 'dca_changes':
@@ -199,7 +42,7 @@ class DcaMappingModel(QAbstractItemModel):
                                             this function will not pick that fact up...
                  Thankfully, creating a cue 'tween two others is not currently possible.
         '''
-        new_cuerow = DcaMapRow(cue, parent=self.root)
+        new_cuerow = ModelsCueRow(cue, parent=self.root)
         self._add_node(self.createIndex(self.root.childCount(), 0, self.root), new_cuerow)
 
         # Set assigns for this cuerow
@@ -207,35 +50,6 @@ class DcaMappingModel(QAbstractItemModel):
 
         # Attach listener so we get cue property changes
         cue.property_changed.connect(self.amend_cuerow)
-
-    def childCount(self, index):
-        node = index.internalPointer() if index.isValid() else self.root
-        return node.childCount()
-
-    def columnCount(self, index):
-        # pylint: disable=no-self-use
-        return 1
-
-    def data(self, index, role=Qt.DisplayRole):
-        if not index.isValid():
-            return None
-        return index.internalPointer().data(role)
-
-    def flags(self, index):
-        if not index.isValid():
-            return None
-        return index.internalPointer().flags
-
-    def index(self, row_num, col_num, parent_idx):
-        if not self.hasIndex(row_num, col_num, parent_idx):
-            return QModelIndex()
-
-        parent_node = parent_idx.internalPointer() if parent_idx.isValid() else self.root
-        child_node = parent_node.child(row_num)
-
-        if child_node:
-            return self.createIndex(row_num, col_num, child_node)
-        return QModelIndex()
 
     def move_cuerow(self, cue, new_cue_index):
         '''Called when a cue is moved in the main cue list'''
@@ -257,7 +71,7 @@ class DcaMappingModel(QAbstractItemModel):
             new_index += 1
 
         self.beginMoveRows(QModelIndex(), old_index, old_index, QModelIndex(), new_index)
-        self.root.children.sort(key=DcaMapRow.value)
+        self.root.children.sort(key=ModelsRow.value)
         self.endMoveRows()
 
         # Update assign entries at the entry point
@@ -276,16 +90,6 @@ class DcaMappingModel(QAbstractItemModel):
         # Finally, cascade changes.
         self._change_tuples_cascade_apply(cuerow)
 
-    def parent(self, index):
-        if not index.isValid():
-            return QModelIndex()
-
-        parent = index.internalPointer().parent
-        if parent == self.root:
-            return QModelIndex()
-
-        return self.createIndex(parent.rownum(), 0, parent)
-
     def remove_cuerow(self, cue):
         '''Removes the cue-row from the model'''
         cue.property_changed.disconnect(self.amend_cuerow)
@@ -298,20 +102,6 @@ class DcaMappingModel(QAbstractItemModel):
         # And remove the cuerow from the model
         self._remove_node(cuerow.index())
 
-    def rowCount(self, index):
-        return self.childCount(index)
-
-    def _add_node(self, destination, new_node):
-        '''Adds a node as a child of another'''
-        ## if destination is an index:
-        parent_index = destination
-        parent_node = parent_index.internalPointer()
-
-        rownum = parent_node.getInsertPoint(new_node.value())
-        self.beginInsertRows(parent_index, rownum, rownum)
-        parent_node.addChild(new_node)
-        self.endInsertRows()
-
     def _change_tuples_apply(self, cuerow, changes):
         for change in copy.copy(changes):
             block_node = cuerow.child(change[0])
@@ -320,7 +110,7 @@ class DcaMappingModel(QAbstractItemModel):
 
             if change[1] not in block_entry_values:
                 if change[2] != AssignStateEnum.UNASSIGN:
-                    self._add_node(block_index, DcaMapEntry(change[1], parent=block_node))
+                    self._add_node(block_index, ModelsEntry(change[1], parent=block_node))
             else:
                 entry_node = block_node.child(block_entry_values.index(change[1]))
                 if entry_node.assign_state() != AssignStateEnum.NONE:
@@ -359,53 +149,12 @@ class DcaMappingModel(QAbstractItemModel):
                                 new_state))
         return new_changes
 
-    def _clear_node(self, node_index):
-        '''Clear a node of all its children'''
-        node = node_index.internalPointer()
-        self.beginRemoveRows(node_index, 0, node.childCount())
-        while node.childCount():
-            node.removeChild(0)
-        self.endRemoveRows()
-
     def _find_cuerow(self, cue_id):
         '''Find and return the cue-row that matches the given cue-id'''
         for cuerow in self.root.children:
             if cuerow.cue.id == cue_id:
                 return cuerow
         return None
-
-    def _relocate_node(self, node_index, destination):
-        '''Relocate a node from its parent node to the end of another '''
-        old_parent_index = node_index.parent()
-        old_parent_node = old_parent_index.internalPointer()
-        old_parent_rownum = node_index.rownum()
-
-        ## if destination is an index:
-        new_parent_index = destination
-        new_parent_node = new_parent_index.internalPointer()
-        ## else if destination is a node:
-        #   new_parent_node = destination
-        #   new_parent_index = self.createIndex(new_parent_node.rownum(), 0, new_parent_node)
-        new_parent_rownum = new_parent_node.getInsertPoint(node_index.internalPointer().value)
-
-        self.beginMoveRows(old_parent_index,
-                           old_parent_rownum,
-                           old_parent_rownum,
-                           new_parent_index,
-                           new_parent_rownum)
-        child = old_parent_node.removeChild(node_index.rownum())
-        child.parent_node = new_parent_node
-        new_parent_node.addChild(child)
-        self.endMoveRows()
-
-    def _remove_node(self, node_index):
-        '''Remove a node from its parent'''
-        if not node_index.isValid():
-            return
-
-        self.beginRemoveRows(self.parent(node_index), node_index.row(), node_index.row())
-        node_index.internalPointer().parent.removeChild(node_index.row())
-        self.endRemoveRows()
 
     def _set_inital_assigns(self, cuerow, cue_defined_assigns, clear_first):
         # Set base add and remove assigns
@@ -418,11 +167,11 @@ class DcaMappingModel(QAbstractItemModel):
 
             for entry in assign_actions['add']:
                 self._add_node(block_index,
-                               DcaMapEntry(entry, AssignStateEnum.ASSIGN, parent=block_node))
+                               ModelsEntry(entry, AssignStateEnum.ASSIGN, parent=block_node))
 
             for entry in assign_actions['rem']:
                 self._add_node(block_index,
-                               DcaMapEntry(entry, AssignStateEnum.UNASSIGN, parent=block_node))
+                               ModelsEntry(entry, AssignStateEnum.UNASSIGN, parent=block_node))
 
         # Get inherits from previous cue row
         if cuerow.prev_sibling():
---
+++ model_primitives.py
@@ -0,0 +1,273 @@
+
+# pylint: disable=missing-docstring
+
+import enum
+
+# pylint: disable=no-name-in-module
+from PyQt5.QtCore import Qt, QAbstractItemModel, QModelIndex
+from PyQt5.QtGui import QBrush, QFont
+from PyQt5.QtWidgets import QApplication
+
+from lisp.plugins import get_plugin
+from lisp.plugins.dca_plotter.utilities import get_mic_assign_name
+
+class AssignStateEnum(enum.Enum):
+    ASSIGN = enum.auto()
+    UNASSIGN = enum.auto()
+    NONE = enum.auto()
+
+### ABSTRACTS
+class ModelsNode():
+    '''Abstract parent class'''
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.flags = Qt.ItemFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+    def data(self, role=Qt.DisplayRole):
+        # pylint: disable=no-self-use, unused-argument
+        return None
+
+    def index(self):
+        return self.model().createIndex(self.rownum(), 0, self)
+
+    def model(self):
+        return self.parent.model()
+
+    def next_sibling(self):
+        if self.rownum() < len(self.parent) - 1:
+            return self.parent.children[self.rownum() + 1]
+        return None
+
+    def prev_sibling(self):
+        if self.rownum():
+            return self.parent.children[self.rownum() - 1]
+        return None
+
+    def rownum(self):
+        if self in self.parent.children:
+            return self.parent.children.index(self)
+        return -1
+
+class ModelsBranchNode(ModelsNode):
+    '''Branch parent class'''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.children = []
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def child(self, child_num):
+        return self.children[child_num]
+
+    def childCount(self):
+        return len(self.children)
+
+    def getChildValues(self):
+        indexes = []
+        for child in self.children:
+            indexes.append(child.value())
+        return indexes
+
+    def getInsertPoint(self, new_value):
+        curr_values = self.getChildValues()
+        curr_values.append(new_value)
+        curr_values.sort()
+        return curr_values.index(new_value)
+
+    def removeChild(self, row):
+        return self.children.pop(row)
+
+    def value(self):
+        if self.rownum() > -1:
+            return self.rownum()
+        return self.parent.childCount()
+
+class ModelsLeafNode(ModelsNode):
+    '''Leaf parent class'''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.flags |= Qt.ItemNeverHasChildren
+
+    def childCount(self):
+        # pylint: disable=no-self-use
+        return 0
+
+### BRANCHES
+class ModelsRootNode(ModelsBranchNode):
+    '''Root class'''
+    def __init__(self, model, **kwargs):
+        super().__init__(**kwargs)
+        self._model = model
+
+    def model(self):
+        return self._model
+
+class ModelsRow(ModelsBranchNode):
+    '''Row class.'''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # pylint: disable=unused-variable
+        for dca in range(get_plugin('DcaPlotter').SessionConfig['dca_count']):
+            self.addChild(ModelsBlock(parent=self))
+
+
+class ModelsBlock(ModelsBranchNode):
+    '''Block class'''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = False
+
+    def addChild(self, child):
+        self.children.insert(self.getInsertPoint(child.value()), child)
+
+    def data(self, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            return 'DCA {}'.format(self.parent.children.index(self) + 1) # @todo: Proper implementation of DCA names
+
+        return super().data(role)
+
+### LEAVES
+class ModelsEntry(ModelsLeafNode):
+    '''Entry class'''
+    def __init__(self, value, state=AssignStateEnum.NONE, **kwargs):
+        super().__init__(**kwargs)
+        self._value = value
+        self._is_inherited = state == AssignStateEnum.NONE
+        self._assign_state = state
+
+    def data(self, role=Qt.DisplayRole):
+        # pylint: disable=too-many-return-statements
+        if role == Qt.DisplayRole:
+            name = get_mic_assign_name(self.value())
+            if self._assign_state == AssignStateEnum.NONE:
+                return "({})".format(name)
+            return name
+
+        if role == Qt.EditRole:
+            return self.value
+
+        if role == Qt.ForegroundRole: # Text colour:
+            if self._assign_state == AssignStateEnum.ASSIGN:
+                return QBrush(Qt.green)
+            if self._assign_state == AssignStateEnum.UNASSIGN:
+                return QBrush(Qt.red)
+            if self._assign_state == AssignStateEnum.NONE:
+                return QBrush(QApplication.palette().dark().color())
+
+        if role == Qt.FontRole and self._assign_state == AssignStateEnum.UNASSIGN:
+            font = QFont()
+            font.setStrikeOut(True)
+            return font
+
+        return super().data(role)
+
+    def assign_state(self):
+        return self._assign_state
+
+    def inherited(self):
+        return self._is_inherited
+
+    def setInherited(self, is_inherited):
+        self._is_inherited = is_inherited
+
+    def value(self):
+        return self._value
+
+### MODEL
+class DcaModelTemplate(QAbstractItemModel):
+    def __init__(self):
+        super().__init__()
+        self.root = ModelsRootNode(model=self)
+
+    def childCount(self, index):
+        node = index.internalPointer() if index.isValid() else self.root
+        return node.childCount()
+
+    def columnCount(self, index):
+        # pylint: disable=no-self-use, unused-argument
+        return 1
+
+    def data(self, index, role=Qt.DisplayRole):
+        # pylint: disable=no-self-use
+        if not index.isValid():
+            return None
+        return index.internalPointer().data(role)
+
+    def flags(self, index):
+        # pylint: disable=no-self-use
+        if not index.isValid():
+            return None
+        return index.internalPointer().flags
+
+    def index(self, row_num, col_num, parent_idx):
+        if not self.hasIndex(row_num, col_num, parent_idx):
+            return QModelIndex()
+
+        parent_node = parent_idx.internalPointer() if parent_idx.isValid() else self.root
+        child_node = parent_node.child(row_num)
+
+        if child_node:
+            return self.createIndex(row_num, col_num, child_node)
+        return QModelIndex()
+
+    def parent(self, index):
+        if not index.isValid():
+            return QModelIndex()
+
+        parent = index.internalPointer().parent
+        if parent == self.root:
+            return QModelIndex()
+
+        return self.createIndex(parent.rownum(), 0, parent)
+
+    def rowCount(self, index):
+        return self.childCount(index)
+
+    def _add_node(self, destination, new_node):
+        '''Adds a node as a child of another'''
+        parent_index = destination
+        parent_node = parent_index.internalPointer()
+
+        rownum = parent_node.getInsertPoint(new_node.value())
+        self.beginInsertRows(parent_index, rownum, rownum)
+        parent_node.addChild(new_node)
+        self.endInsertRows()
+
+    def _clear_node(self, node_index):
+        '''Clear a node of all its children'''
+        node = node_index.internalPointer()
+        self.beginRemoveRows(node_index, 0, node.childCount())
+        while node.childCount():
+            node.removeChild(0)
+        self.endRemoveRows()
+
+    def _relocate_node(self, node_index, destination):
+        '''Relocate a node from its parent node to the end of another '''
+        old_parent_index = node_index.parent()
+        old_parent_node = old_parent_index.internalPointer()
+        old_parent_rownum = node_index.rownum()
+
+        new_parent_index = destination
+        new_parent_node = new_parent_index.internalPointer()
+        new_parent_rownum = new_parent_node.getInsertPoint(node_index.internalPointer().value)
+
+        self.beginMoveRows(old_parent_index,
+                           old_parent_rownum,
+                           old_parent_rownum,
+                           new_parent_index,
+                           new_parent_rownum)
+        child = old_parent_node.removeChild(node_index.rownum())
+        child.parent_node = new_parent_node
+        new_parent_node.addChild(child)
+        self.endMoveRows()
+
+    def _remove_node(self, node_index):
+        '''Remove a node from its parent'''
+        if not node_index.isValid():
+            return
+
+        self.beginRemoveRows(self.parent(node_index), node_index.row(), node_index.row())
+        node_index.internalPointer().parent.removeChild(node_index.row())
+        self.endRemoveRows()
